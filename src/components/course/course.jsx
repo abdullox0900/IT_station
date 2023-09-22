f1b0363cc4ccf3abc5ada37a2a46bdbfc29b951f
zednegr <negrzed@gmail.com>
2023-09-22 12:32:18 +0500

    animaton

--- src/components/course/course.jsx
+++ src/components/course/course.jsx
@@ -9,77 +9,85 @@ import CourseCardImg from "../../assets/svg/course-card-img.svg"
 export default function Course() {
     return (
         <CourseSection>
-                <CourseStart>
-                    <CourseStartH3>Yo'nalishlar bo'yicha kurslar</CourseStartH3>
-                </CourseStart>
+            <CourseStart>
+                <CourseStartH3 data-aos="fade-right" data-aos-duration="1000">Yo'nalishlar bo'yicha kurslar</CourseStartH3>
+            </CourseStart>
 
-                <CourseWrapper>
-                    
-                    <CourseCard>
-                        <CourseCardLeft>
-                            <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
-                            <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
-                                dasturlash tillari va ular orqali dasturlashning
-                                mantiq-mohiyati o'rgatiladi. Kurs davomida
-                                dasturlashning fundamental tushunchalari,
-                                dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
-                            <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
-                        </CourseCardLeft>
+            <CourseWrapper>
 
-                        <CourseCardRight>
-                            <CourseCardRightImg src={CourseCardImg} alt='Network error'/>
-                        </CourseCardRight>
-                    </CourseCard>
+                <CourseCard data-aos="fade-right" data-aos-duration="1000">
 
-                    <CourseCard>
-                        <CourseCardLeft>
-                            <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
-                            <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
-                                dasturlash tillari va ular orqali dasturlashning
-                                mantiq-mohiyati o'rgatiladi. Kurs davomida
-                                dasturlashning fundamental tushunchalari,
-                                dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
-                            <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
-                        </CourseCardLeft>
+                    <CourseCardLeft>
+                        <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
+                        <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
+                            dasturlash tillari va ular orqali dasturlashning
+                            mantiq-mohiyati o'rgatiladi. Kurs davomida
+                            dasturlashning fundamental tushunchalari,
+                            dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
+                        <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
+                    </CourseCardLeft>
 
-                        <CourseCardRight>
-                            <CourseCardRightImg src={CourseCardImg} alt='Network error'/>
-                        </CourseCardRight>
-                    </CourseCard>
+                    <CourseCardRight>
+                        <CourseCardRightImg src={CourseCardImg} alt='Network error' />
+                    </CourseCardRight>
 
-                    <CourseCard>
-                        <CourseCardLeft>
-                            <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
-                            <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
-                                dasturlash tillari va ular orqali dasturlashning
-                                mantiq-mohiyati o'rgatiladi. Kurs davomida
-                                dasturlashning fundamental tushunchalari,
-                                dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
-                            <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
-                        </CourseCardLeft>
+                </CourseCard>
 
-                        <CourseCardRight>
-                            <CourseCardRightImg src={CourseCardImg} alt='Network error'/>
-                        </CourseCardRight>
-                    </CourseCard>
+                <CourseCard data-aos="fade-left" data-aos-duration="1000">
 
-                    <CourseCard>
-                        <CourseCardLeft>
-                            <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
-                            <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
-                                dasturlash tillari va ular orqali dasturlashning
-                                mantiq-mohiyati o'rgatiladi. Kurs davomida
-                                dasturlashning fundamental tushunchalari,
-                                dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
-                            <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
-                        </CourseCardLeft>
+                    <CourseCardLeft>
+                        <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
+                        <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
+                            dasturlash tillari va ular orqali dasturlashning
+                            mantiq-mohiyati o'rgatiladi. Kurs davomida
+                            dasturlashning fundamental tushunchalari,
+                            dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
+                        <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
+                    </CourseCardLeft>
 
-                        <CourseCardRight>
-                            <CourseCardRightImg src={CourseCardImg} alt='Network error'/>
-                        </CourseCardRight>
-                    </CourseCard>
+                    <CourseCardRight>
+                        <CourseCardRightImg src={CourseCardImg} alt='Network error' />
+                    </CourseCardRight>
 
-                </CourseWrapper>
+                </CourseCard>
+
+                <CourseCard data-aos="fade-right" data-aos-duration="1000">
+
+                    <CourseCardLeft>
+                        <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
+                        <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
+                            dasturlash tillari va ular orqali dasturlashning
+                            mantiq-mohiyati o'rgatiladi. Kurs davomida
+                            dasturlashning fundamental tushunchalari,
+                            dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
+                        <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
+                    </CourseCardLeft>
+
+                    <CourseCardRight>
+                        <CourseCardRightImg src={CourseCardImg} alt='Network error' />
+                    </CourseCardRight>
+
+                </CourseCard>
+
+                <CourseCard data-aos="fade-left" data-aos-duration="1000">
+
+                    <CourseCardLeft>
+                        <CourseCardLeftName>Dasturlash foundation</CourseCardLeftName>
+                        <CourseCardLeftInfo>Foundation dasturlash kursida C va Python
+                            dasturlash tillari va ular orqali dasturlashning
+                            mantiq-mohiyati o'rgatiladi. Kurs davomida
+                            dasturlashning fundamental tushunchalari,
+                            dasturchining ishini yengillashtirib beradigan</CourseCardLeftInfo>
+                        <CourseCardLeftButton>Dasturlash</CourseCardLeftButton>
+                    </CourseCardLeft>
+
+                    <CourseCardRight>
+                        <CourseCardRightImg src={CourseCardImg} alt='Network error' />
+                    </CourseCardRight>
+
+                </CourseCard>
+
+            </CourseWrapper>
         </CourseSection>
     )
 }
